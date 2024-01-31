1350ec305d68d5e0dd7a6af206be047848fecb8b
usama-666 <ejazali8582@gmail.com>
2024-01-31 15:37:37 +0500

    added updated backend url

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a new Axios instance with a base URL
 const API = axios.create({
-  baseURL: "https://soul-club-niftyzone-final.vercel.app/api/v1", // Your base URL here
+  baseURL: "https://soulclub-niftyzone.vercel.app/api/v1", // Your base URL here
   // baseURL: "http://localhost:5000/api/v1", // Your base URL here
 
   withCredentials: true,
